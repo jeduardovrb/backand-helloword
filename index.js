de42936557298f65086408cc8531da9389b99fac
unknown <jorgeeduardovrb@gmail.com>
2021-03-16 21:47:28 -0300

    ajustes projeto.

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
 const port = 3000;
+
+// então, criamos uma rota para '/'
+app.get('/', (req, res) => {
+  // aqui precisamos enviar o index.html para o cliente
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+
+app.get('/index.html', (req, res) => {
+    let acesso_url = (__dirname + '/src/index.html');
+    res.sendFile(path.join(acesso_url)); 
+});
 
 app.get('/ab[0-9]cd', (req, res) => {
     res.send("Essa é uma expressão regular. ");
